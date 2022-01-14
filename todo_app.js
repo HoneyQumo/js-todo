35fe387b6ba83adb0f58a62823c6b14f7478afbf
nikita <honeyqumo@gmail.com>
2022-01-15 00:00:07 +0300

    todo_app v.0.0.2 | Исправил toggle выбранных дел, исправил localStorage, теперь данные корректно подгружаются со 'Storage', исправил удаление, теперь корректно работает.

--- todo_app.js
+++ todo_app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 
-const listCase = document.getElementsByClassName('list__case');
+
 
 
 function init() {
@@ -10,57 +10,63 @@ function init() {
         let fromStorage = `<li class="list__case">${localStorage[i]}</li>`;
         list.insertAdjacentHTML('beforeend', fromStorage);
     }
+    actionAddCase();
     drawCloseButton();
     toggleCheckedClass();
     deleteCase();
+
 }
 init();
 
 
 function drawCloseButton() {
-
-    for (const item of listCase) {
-
+    const listCase = document.querySelectorAll('.list__case');
+    listCase.forEach(el => {
         const deleteButton = document.createElement('span');
         const drawCross = document.createTextNode('×');
 
         deleteButton.append(drawCross);
         deleteButton.classList.add('close');
+        el.append(deleteButton);
+    });
 
-        item.append(deleteButton);
-    }
+
 }
 
 
 function toggleCheckedClass() {
-    for (const item of listCase) {
-        item.addEventListener('click', (event) => {
-            if (event.target.className !== 'close') {
-                item.classList.toggle('list__checked');
-            }
-        });
-
-    }
+    let listCase = document.querySelectorAll('.list__case');
+    // listCase.forEach(el => {
+    //     el.addEventListener('click', (e) => {
+    //         e.target.classList.toggle('list__checked');
+    //     });
+    // });
+    listCase.forEach(el => {
+        if (!el.classList.contains('toggle')) {
+            el.classList.add('toggle');
+            el.addEventListener('click', (e) => {
+                e.target.classList.toggle('list__checked');
+            });
+        }
+    });
 }
 
-// toggleCheckedClass();
 
 
 function createCase(text) {
-    return `<li class="list__case">${text}</li>`;
+    return `<li class="list__case">${text}<span class="close">×</span></li>`;
 }
 
 
 function addCase() {
     const list = document.querySelector('.list');
     const input = document.querySelector('.header__input');
+
     if (input.value != '') {
         list.insertAdjacentHTML('beforeend', createCase(input.value));
-        drawCloseButton();
+        localStorage.setItem(localStorage.length, input.value);
         toggleCheckedClass();
         deleteCase();
-
-        localStorage.setItem(localStorage.length, input.value);
     }
     input.value = '';
 }
@@ -71,26 +77,46 @@ function actionAddCase() {
     addButton.addEventListener('click', addCase);
 }
 
-actionAddCase();
 
+// function deleteCase() {
+//     const closeButtons = document.getElementsByClassName('close');
+//     for (const list of listCase) {
+//         list.addEventListener('click', (event) => {
+//             for (let i = 0; i < closeButtons.length; i++) {
+//                 if (closeButtons[i] === event.target) {
+//                     list.remove();
+//                 }
+//                 // if (event.target.parentNode.textContent.slice(0, event.target.parentNode.textContent.length - 1) === localStorage[i]) {
+//                 // localStorage.removeItem(i);
+//                 // }
+//                 // if (event.target.parentNode.textContent.slice(0, event.target.parentNode.textContent.length - 1) === list.textContent.slice(0, list.textContent.length - 1)) {
+//                 //     console.log(localStorage.key(i));
+//                 // }
+//             }
+//             for (let ind = 0; ind < localStorage.length; ind++) {
+//                 if (event.target.parentNode.textContent.slice(0, event.target.parentNode.textContent.length - 1) === localStorage.getItem(ind)) {
+//                     localStorage.removeItem(ind);
+//                 }
+//             }
+//         });
+//     }
+// }
 
 function deleteCase() {
-    const closeButtons = document.getElementsByClassName('close');
+    const cases = document.querySelectorAll('.list__case');
+    const closeButtons = document.querySelectorAll('.close');
 
-    for (const list of listCase) {
-        list.addEventListener('click', (event) => {
-
-            for (let i = 0; i < closeButtons.length; i++) {
-                if (closeButtons[i] === event.target) {
-                    list.remove();
-                }
-                if (event.target.parentNode.textContent.slice(0, event.target.parentNode.textContent.length - 1) === localStorage[i]) {
+    cases.forEach(caseis => {
+        caseis.addEventListener('click', (e) => {
+            for (let i = 0; i < cases.length; i++) {
+                if (closeButtons[i] === e.target) {
+                    caseis.remove();
                     localStorage.removeItem(i);
                 }
-
             }
         });
-    }
+    });
+
 }
 
 
